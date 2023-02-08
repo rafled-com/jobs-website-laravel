fa1f731258f737608379554a3679ebcf0c438f9b
shreejalmaharjan-27 <shreejal@rafled.com>
2023-02-08 15:04:29 +0000

    create job posty controller

---
+++ app/Http/Controllers/JobPostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JobPostController extends Controller
+{
+    public function view()
+    {
+        return view('post_job');
+    }
+}
