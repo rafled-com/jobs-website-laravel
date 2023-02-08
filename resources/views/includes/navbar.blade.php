8c73784f8ff11c096612562d8660d82a9bd7e690
shreejalmaharjan-27 <shreejal@rafled.com>
2023-02-08 15:04:46 +0000

    hide post jobs when user is in hire page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -12,9 +12,12 @@
                     <x-bx-user class="w-7 h-7" />
                 </button>
             @endif
-            <button type="button" class="text-white bg-light_primary hover:bg-light_hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-dark_primary dark:hover:bg-dark_hover dark:focus:ring-blue-800">
-                Post Jobs
-            </button>
+
+            @if(Route::currentRouteName() != 'hire')
+                <button type="button" class="text-white bg-light_primary hover:bg-light_hover focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-dark_primary dark:hover:bg-dark_hover dark:focus:ring-blue-800">
+                    Post Jobs
+                </button>
+            @endif
             <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
